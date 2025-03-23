91e115da9fd3bef497a78aae27ab71cf971102b6
at997 <at997@students.waikato.ac.nz>
2025-03-24 09:34:17 +1300

    Dfinal

--- getWeather.php
+++ getWeather.php
@@ -5,7 +5,7 @@ $lat = isset($_GET['lat']) ? $_GET['lat'] : 0;
 
 
 
-$url = "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apikey";
+$url = "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=c137c7e47aa39372166fce1c4d9cd606";
 
 $json = file_get_contents($url);
 echo $json;
--- index.js
+++ index.js
@@ -11,134 +11,124 @@
 // }
 
 function formatSet(obj) {
-  console.log(Object.keys(obj));
-
   return Object.keys(obj)
     .map((key) => key + ' = "' + obj[key] + '"')
     .join(",");
 }
 
-fetch("events.php?eventID=all", { mode: "no-cors" })
-  .then((events) => events.json())
-  .then((events) => {
-    const eventTable = document.getElementById("eventTable");
+(function dataColeciton() {
+  fetch("events.php?eventID=all", { mode: "no-cors" })
+    .then((events) => events.json())
+    .then((events) => {
+      const eventTable = document.getElementById("eventTable");
+      eventTable.innerHTML = "";
 
-    console.log(events);
+      console.log(events);
 
-    const dataToDisplay = ["name", "location"];
-    events.forEach((event) => {
-      const row = document.createElement("tr");
-      dataToDisplay.forEach((data) => {
-        const cell = document.createElement("td");
-        cell.textContent = event[data];
+      const dataToDisplay = ["name", "location"];
+      events.forEach((event) => {
+        const row = document.createElement("tr");
 
-        if (data === "name") {
-          const eventDetails = document.getElementById("eventDetails");
-          const weatherInfo = document.getElementById("weatherInfo");
-          cell.addEventListener("click", () => {
+        const eventDetails = document.getElementById("eventDetails");
+        const weatherInfo = document.getElementById("weatherInfo");
+
+        row.addEventListener("click", () => {
+          weatherInfo.innerHTML = "";
+          eventDetails.style.display = "block";
+          console.log(event);
+          eventDetails.innerHTML = "";
+
+          Object.keys(event)
+            .sort()
+            .forEach((key) => {
+              const detail = document.createElement("tr");
+              detail.classList.add("eventDetail");
+
+              const detailKey = document.createElement("td");
+              detailKey.classList.add("detailKey");
+              detailKey.innerHTML = key;
+
+              const detailValue = document.createElement("td");
+              detailValue.classList.add("detailValue");
+              detailValue.setAttribute("contenteditable", "true");
+              detailValue.innerHTML = event[key];
+
+              detail.appendChild(detailKey);
+              detail.appendChild(detailValue);
+
+              eventDetails.appendChild(detail);
+            });
+
+          const icon = document.createElement("button");
+          icon.innerHTML = "Get Weather";
+          icon.addEventListener("click", () => {
+            weatherInfo.style.display = "block";
             weatherInfo.innerHTML = "";
-            eventDetails.style.display = "block";
-            console.log(event);
-            eventDetails.innerHTML = "";
+            let [lat, lon] = event["lon_lat"].split(",");
+            lat = lat.trim();
+            lon = lon.trim();
+            fetch("getWeather.php?lat=" + lat + "&lon=" + lon)
+              .then((weather) => weather.json())
+              .then((weather) => {
+                console.log(weather);
+                const weatherDetails = ["temp", "humidity", "weather"];
+                weatherDetails.forEach((weatherDetail) => {
+                  const detail = document.createElement("p");
 
-            Object.keys(event)
-              .sort()
-              .forEach((key) => {
-                const detail = document.createElement("tr");
-                detail.classList.add("eventDetail");
+                  detail.textContent = `${weatherDetail}: ${weather.main[weatherDetail]}`;
 
-                const detailKey = document.createElement("td");
-                detailKey.classList.add("detailKey");
-                detailKey.innerHTML = key;
-
-                const detailValue = document.createElement("td");
-                detailValue.classList.add("detailValue");
-                detailValue.setAttribute("contenteditable", "true");
-                detailValue.innerHTML = event[key];
-
-                detail.appendChild(detailKey);
-                detail.appendChild(detailValue);
-
-                eventDetails.appendChild(detail);
-              });
-
-            const svgns = "http://www.w3.org/2000/svg";
-            const icon = document.createElementNS(svgns, "svg");
-            icon.innerHTML =
-              '<circle cx="25" cy="25" r="24" stroke="green" fill="yellow" />';
-            icon.setAttribute("height", "50");
-            icon.setAttribute("width", "50");
-            icon.addEventListener("click", () => {
-              weatherInfo.style.display = "block";
-              weatherInfo.innerHTML = "";
-              let [lat, lon] = event["lon_lat"].split(",");
-              lat = lat.trim();
-              lon = lon.trim();
-              fetch("getWeather.php?lat=" + lat + "&lon=" + lon)
-                .then((weather) => weather.json())
-                .then((weather) => {
-                  console.log(weather);
-                  const weatherDetails = ["temp", "humidity", "weather"];
-                  weatherDetails.forEach((weatherDetail) => {
-                    const detail = document.createElement("p");
-
-                    detail.textContent = `${weatherDetail}: ${weather.main[weatherDetail]}`;
-
-                    if (weatherDetail === "weather") {
-                      detail.textContent = `${weatherDetail}: ${weather.weather[0].description}`;
-                    }
-
-                    weatherInfo.appendChild(detail);
-                  });
-                });
-            });
-            eventDetails.appendChild(icon);
-
-            const saveButton = document.createElement("button");
-            saveButton.innerHTML = "Save";
-            saveButton.addEventListener("click", () => {
-              const keys = document.getElementsByClassName("detailKey");
-              let values = document.getElementsByClassName("detailValue");
-              let changedValues = {};
-              for (let i = 0; i < keys.length; i++) {
-                if (values[i].innerHTML !== event[keys[i].innerHTML]) {
-                  if (values[i].innerHTML.includes("<br>")) {
-                    values[i].innerHTML = values[i].innerHTML.replace(
-                      "<br>",
-                      ""
-                    );
+                  if (weatherDetail === "weather") {
+                    detail.textContent = `${weatherDetail}: ${weather.weather[0].description}`;
                   }
-                  changedValues[keys[i].innerHTML] = values[i].innerHTML.trim();
-                  event[keys[i].innerHTML] = changedValues[keys[i].innerHTML];
-                }
-              }
-              let set = formatSet(changedValues);
 
-              console.log(set);
-              fetch("events.php", {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/x-www-form-urlencoded",
-                },
-                body: new URLSearchParams({
-                  set: set,
-                  id: event.id,
-                }),
-              })
-                .then((response) => response.text())
-                .then((response) => console.log(response))
-                .then(() => {
-                  cell.innerHTML = changedValues.name;
+                  weatherInfo.appendChild(detail);
                 });
-            });
-
-            eventDetails.appendChild(saveButton);
+              });
           });
-        }
+          eventDetails.appendChild(icon);
 
-        row.appendChild(cell);
-        event.data = cell;
+          const saveButton = document.createElement("button");
+          saveButton.innerHTML = "Save";
+          saveButton.addEventListener("click", () => {
+            const keys = document.getElementsByClassName("detailKey");
+            let values = document.getElementsByClassName("detailValue");
+            let changedValues = {};
+            for (let i = 0; i < keys.length; i++) {
+              if (values[i].innerHTML !== event[keys[i].innerHTML]) {
+                if (values[i].innerHTML.includes("<br>")) {
+                  values[i].innerHTML = values[i].innerHTML.replace("<br>", "");
+                }
+                changedValues[keys[i].innerHTML] = values[i].innerHTML.trim();
+                event[keys[i].innerHTML] = changedValues[keys[i].innerHTML];
+              }
+            }
+            let set = formatSet(changedValues);
+
+            console.log(set);
+            fetch("events.php", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+              },
+              body: new URLSearchParams({
+                set: set,
+                id: event.id,
+              }),
+            })
+              .then((response) => response.text())
+              .then((response) => console.log(response))
+              .then(dataColeciton());
+          });
+
+          eventDetails.appendChild(saveButton);
+        });
+
+        dataToDisplay.forEach((data) => {
+          const cell = document.createElement("td");
+          cell.innerHTML = event[data];
+          row.appendChild(cell);
+        });
+        eventTable.appendChild(row);
       });
-      eventTable.appendChild(row);
     });
-  });
+})();
